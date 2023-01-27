0fec2fd51163ee22ffe66b695c758365e499f851
mazharul <mazharulislam1998r@gmail.com>
2023-01-27 11:43:26 +0600

    mess fix index.js and add App.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Card from './components/Card';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Card />
+  <App />
 );
