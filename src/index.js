c49a44478b89fb35da8f643806601a8e5f6b8b3d
mazharul <mazharulislam1998r@gmail.com>
2023-01-27 10:12:09 +0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const appTitle = 'Todo App';
+const todoTitle = 'Call Family';
+const todoDescription = 'This is todo description';
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const currentYear = date.getFullYear();
+
+const headingStyle = {color: "red", fontSize: "50px", backgroundColor: "yellow"};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <div>
+    <h1 className='headingStyle largeText'>{appTitle}</h1>
+    <h3>{todoTitle}</h3>
+    <p>{todoDescription}</p>
+    <p>{dateName + " " + monthName + " " + currentYear}</p>
+  </div>
+);
